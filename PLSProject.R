2f305048a4ee34b31a566e02cedeea8757a9e544
Ricardo Avila <ricardo.avila@rye.mx>
2017-05-14 11:59:12 +0100

    Create functions

--- PLSProject.R
+++ PLSProject.R
@@ -1,54 +1,115 @@
+############# CLEAN GLOBAL ENVIRONMENT #########
+rm(list=ls(envir = .GlobalEnv), envir = .GlobalEnv)
 
-rm(list=ls())
 
-# Set the directory
-setwd("C:\\Users\\Ricardo\\Sync\\NOVA\\Second semester\\Descriptive analytics - Estadistica l\\Final project")
+############# FUNCTIONS ###############
+defineEnvironment <- function(file_directory="C:\\Users\\",file_name ){
+  #Working directory
+  setwd(file_directory)
+  #read and discover the data 
+  read_file = read.csv(file_name)
 
-#read and discover the data 
-bank = read.csv("bank.csv")
-summary(bank)
+  return(read_file)
+}
 
-#Remove the missing rows from the dataset
-bank2 <- bank[rowSums(is.na(bank)) != ncol(bank)-1, ]
-summary(bank2)
-#check how many variables are missing in total 
-nrow(bank) - nrow(bank2)
+printResults <- function(dataSet,message=""){
+  print(message)
+  print(c("CLASS", class(dataSet)))
+  print(c("Total rows", nrow(dataSet)))
+  print(c("Total Columns", ncol(dataSet)))
+  print(c("Number of complete rows", length(which(complete.cases(dataSet)==TRUE))))
+  print(c("Number of rows with missing values", length(which(complete.cases(dataSet)==FALSE))))
+  #summary(dataSet)
+}
 
-#Clean the ID column
-bank2$ID = 1:nrow(bank2)
+removeEmptyRows <- function(dataSet){
+  #Remove the missing rows from the dataset
+  dataSetClean <- dataSet[rowSums(is.na(dataSet)) != ncol(dataSet)-1, ]
+  #check how many variables are missing in total 
+  print(c("Empty rows deleted:",nrow(dataSet) - nrow(dataSetClean)))
+  return(dataSetClean)
+}
 
-#Imputing missing values with pmm method and with R package of Mice
-install.packages("mice")
-library(mice)
+generateIdColumn <- function(dataSet){
+  #Clean the ID column
+  dataSet$ID = 1:nrow(dataSet)
+  return(dataSet)
+}
 
-#The data set with different imputations of missing values 
-TempMiss = mice(bank2, m=10, method = "pmm", maxit = 50, seed = 500 )
+imputeMissingValues <- function(dataSet,method="pmm"){
+  #Imputing missing values with pmm method and with R package of Mice
+  library(mice)
+  
+  #The data set with different imputations of missing values 
+  TempMiss = mice(dataSet, m=10, method = "pmm", maxit = 50, seed = 500 )
+  
+  #some variable imputations (each with 10 imputation values for each missing value)
+  TempMiss$imp$IMAG1
+  TempMiss$imp$IMAG2
+  
+  #Imputed bank data set with the first imputation among the 10
+  ImputedDataSet = complete(TempMiss)
+  ImputedDataSet = ImputedDataSet[,c(2:ncol(ImputedDataSet))]
+  return(ImputedDataSet)
+  
+}
 
-#some variable imputations (each with 10 imputation values for each missing value)
-TempMiss$imp$IMAG1
-TempMiss$imp$IMAG2
+buildModel <- function(values){
+  newModel = matrix(values, ncol=2, byrow = TRUE)
+  colnames(newModel) = c("Source", "Target")
+  print(newModel)
+  return(newModel)
+}
 
-#Imputed bank data set with the first imputation among the 10
-ImputedBank = complete(TempMiss)
-ImputedBank = ImputedBank[,c(2:25)]
-summary(ImputedBank)
+matrixInitial <- function(rows,cols,name_rows,name_cols){
+  newMatrix = matrix(c(rep(0,rows*cols)), nrow = rows, ncol = cols)
+  colnames(newMatrix) = name_rows
+  rownames(newMatrix) = name_cols
+  print(newMatrix)
+  return(newMatrix)
+}
+
+zeroInitialize <- function(rows,cols=1){
+  newVector = c(rep(0,rows))
+  return(newVector)
+}
+
+######## GLOBAL PARAMETERS ########
+
+file = "bank.csv"
+file_directory = "C:\\Users\\Ricardo\\Sync\\NOVA\\Second semester\\Descriptive analytics - Estadistica l\\Final project"
+
+###################################
+
+bank = defineEnvironment(file_directory ,file )
+printResults(bank,"First Bank dataset")
+
+bank2 = removeEmptyRows(bank)
+printResults(bank2,"Without empty rows Bank dataset")
+
+bank2 = generateIdColumn(bank2)
+printResults(bank2,"Final")
+
+ImputedBank = imputeMissingValues(bank2,"pmm")
+printResults(ImputedBank,"ImputedBank")
+#Standardize the data
+ImputedBankStd = scale(ImputedBank)
+
+printResults(ImputedBankStd,"ImputedBankStd")
+
 
 # Building the Inner Model
-InModel = matrix(c("Image", "Loyalty", "Image", "Expectations", "Image",
-                   "Satisfaction", "Expectations", "Quality", "Expectations", "Value", "Expectations", "Satisfaction", "Quality",
-                   "Satisfaction", "Quality", "Value", "Value", "Satisfaction", "Satisfaction", "Loyalty"), ncol=2, byrow = TRUE)
-InModel
-
-colnames(InModel) = c("Source", "Target")
+InModel = buildModel(c("Image", "Loyalty", "Image", "Expectations", "Image",
+                       "Satisfaction", "Expectations", "Quality", "Expectations", "Value", "Expectations", "Satisfaction", "Quality",
+                       "Satisfaction", "Quality", "Value", "Value", "Satisfaction", "Satisfaction", "Loyalty"))
 
 # Building the Outer Model
-OutModel = matrix(c("Image", "IMAG1", "Image", "IMAG2","Image", "IMAG3",	"Image", "IMAG4","Image", "IMAG5"
-                    , "Expectations","EXPE1",	"Expectations", "EXPE2", "Expectations", "EXPE3",
-                    "Quality", "QUAL1",	"Quality", "QUAL2",	"Quality", "QUAL3", "Quality", "QUAL4",	"Quality", "QUAL5",
-                    "Quality", "QUAL6", "Quality", "QUAL7", "Quality", "QUAL8", "Quality","QUAL9", "Value",	"VALU1","Value","VALU2",
-                    "Satisfaction","SATI1", "Satisfaction", "SATI2" ,"Satisfaction", "SATI3", "Loyalty", "LOYA1", "Loyalty",	"LOYA2"),ncol=2,byrow = TRUE)
-OutModel
-colnames(OutModel) = c("Source", "Target")
+OutModel = buildModel(c("Image", "IMAG1", "Image", "IMAG2","Image", "IMAG3",	"Image", "IMAG4","Image", "IMAG5"
+                       , "Expectations","EXPE1",	"Expectations", "EXPE2", "Expectations", "EXPE3",
+                       "Quality", "QUAL1",	"Quality", "QUAL2",	"Quality", "QUAL3", "Quality", "QUAL4",	"Quality", "QUAL5",
+                       "Quality", "QUAL6", "Quality", "QUAL7", "Quality", "QUAL8", "Quality","QUAL9", "Value",	"VALU1","Value","VALU2",
+                       "Satisfaction","SATI1", "Satisfaction", "SATI2" ,"Satisfaction", "SATI3", "Loyalty", "LOYA1", "Loyalty",	"LOYA2"))
+
 
 # Preaparing the Model
 LatentVariables = unique(OutModel[,1])
@@ -57,67 +118,117 @@ NbLatentVariables = length(LatentVariables)
 NbManifestVariables = length(ManifestVariables)
 
 #Building Pls wheights and paths coefficient Matrices
-PlsWeights = matrix(rep(0,NbManifestVariables*NbLatentVariables), nrow = NbManifestVariables, ncol = NbLatentVariables)
-colnames(PlsWeights) = LatentVariables
-rownames(PlsWeights) = ManifestVariables
+PlsWeights =  matrixInitial(NbManifestVariables,NbLatentVariables,LatentVariables,ManifestVariables)
+PathCoeffs = matrixInitial(NbLatentVariables,NbLatentVariables,LatentVariables,LatentVariables)
 
-PathCoeffs = matrix(rep(0,NbLatentVariables*NbLatentVariables), nrow = NbLatentVariables, ncol = NbLatentVariables)
-colnames(PathCoeffs) = LatentVariables
-rownames(PathCoeffs) = LatentVariables
+#Represent the connections between the latent variables
+LatentConnections = PathCoeffs 
+for(i in 1:nrow(InModel)){
+      a = which(LatentVariables==InModel[i,1])
+      b = which(LatentVariables==InModel[i,2])
+      LatentConnections[a,b] = 1
+      LatentConnections[b,a] = 1
+}
 
-
-#Vector of occurences of each latentVariable
-NbManifestPerLatent = c(rep(0,length(LatentVariables)))
+#Verctor of occurences of each latentVariable
+NbManifestPerLatent = zeroInitialize(NbLatentVariables)
 for(i in 1:length(LatentVariables)){
   NbManifestPerLatent[i] = c(length(grep(LatentVariables[i], OutModel[,1])));
+}
+
+
+#Corresponding data set for each Latent Variable
+BankSetPerLatent = list()
+minElement = 1
+for(i in 1:NbLatentVariables){
+  maxElement = minElement + NbManifestPerLatent[i] -1
+  BankSetPerLatent[[i]] = ImputedBankStd[,c(minElement:maxElement)]
+  minElement = minElement + NbManifestPerLatent[i]
 }
 
 #Temporary Vector of weights
 WeightsTemp = c(rep(1/24,24))
 
 
-#List of Weights Per Latent variable
-WeightsTemp2 = WeightsTemp
-WeightsPerLatent = list()
-for(i in 1:NbLatentVariables){
-  WeightsPerLatent[[i]] = WeightsTemp2[1:NbManifestPerLatent[i]]
-  WeightsTemp2 = WeightsTemp2[-c(1:NbManifestPerLatent[i])]
-}
+#************For****************************************
 
-#Corresponding data set for each Latent Variable
-BankSetPerLatent = list()
-CopyImputedBank = ImputedBank
-for(i in 1:NbLatentVariables){
-  BankSetPerLatent[[i]] = CopyImputedBank[,c(1:NbManifestPerLatent[i])]
-  CopyImputedBank = CopyImputedBank[-c(1:NbManifestPerLatent[i])]
-}
+Iteration = 0
 
-#Build Y1...Yn
-Y = list()
-for(i in 1:length(BankSetPerLatent)){
-  Y[[i]] = c(rep(0,nrow(ImputedBank)))
-  for(j in 1:length(WeightsPerLatent[[i]])){
-    Y[[i]] = Y[[i]] + WeightsPerLatent[[i]][j]*BankSetPerLatent[[i]][,j]
+repeat{
+
+
+  #List of Weights Per Latent variable
+  minElement = 1
+  WeightsPerLatent = list()
+  for(i in 1:NbLatentVariables){
+    maxElement = minElement + NbManifestPerLatent[i] -1
+    WeightsPerLatent[[i]] = WeightsTemp[minElement:maxElement]
+    minElement = minElement + NbManifestPerLatent[i]
   }
-}
-
-#Build the inner weights ei
-e=c()
-for(i in 1:length(Y)){
-  for(j in 1:length(Y)){
-    if(i!=j){
-      e = c(e,cor(Y[[i]],Y[[j]]))
+  
+  #Build Y1...Yn
+  Y = list()
+  for(i in 1:length(BankSetPerLatent)){
+    Y[[i]] = zeroInitialize(nrow(ImputedBank))
+    for(j in 1:length(WeightsPerLatent[[i]])){
+      Y[[i]] = Y[[i]] + WeightsPerLatent[[i]][j]*BankSetPerLatent[[i]][,j]
     }
   }
-}
-
-#Build the Zeds z
-Z = list();
-Hide=1
-for(i in 1:length(Y)){
-  if (i != Hide){
-    
+  
+  InModel
+  
+  #Build the inner weights ei
+  e=c()
+  for(i in 1:length(Y)){
+    for(j in 1:length(Y)){
+      if(i!=j){
+        if(LatentConnections[i,j]==1){
+          e = c(e,cor(Y[[i]],Y[[j]]))
+        }
+        else{
+          e=c(e,0)
+        }
+      }
+      
+    }
+  }
+  #Build the Zeds z
+  z=list()
+  for(i in 1:length(BankSetPerLatent)){
+    z[[i]] = c(rep(0,nrow(ImputedBank)))
+  }
+  
+  
+  aux=1
+  for(i in 1:length(Y)) {
+    for(j in 1:length(Y)){
+      if(j!=i){
+        z[[i]]=z[[i]]+Y[[j]]*e[aux]
+        aux=aux+1
+      }
+    }
+  }
+  
+  #UpdateWeights
+  WeightsTempNew = WeightsTemp
+  WeightNewPerLatent = WeightsPerLatent
+  k=0
+  for(i in 1:length(LatentVariables)){
+    for(j in 1:NbManifestPerLatent[i]){
+      k=k+1
+      WeightNewPerLatent[[i]][j] = cov(z[[i]],BankSetPerLatent[[i]][,j])
+      WeightsTempNew[k] = cov(z[[i]],BankSetPerLatent[[i]][,j])
+    }
+  }
+  
+  epsilon = sqrt(sum((WeightsTempNew - WeightsTemp)^2))
+  WeightsTemp = WeightsTempNew
+  Iteration = Iteration +1
+  print(Iteration)
+  if(epsilon <= 2){
+    break
   }
 }
+#**********************************For****************************
 
 
